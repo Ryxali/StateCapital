79c22870172bc3e83896224e44e8e351c6ff7eba
Niklas Lindblad <ryxali@gmail.com>
2015-12-12 17:11:38 +0100

    amazing slider action

--- Assets/CityBlock.cs
+++ Assets/CityBlock.cs
@@ -25,7 +25,7 @@ public class CityBlock : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         leaning += Game.leaning * Time.deltaTime * 0.1f;
-
+        Game.leaningAggregate += leaning;
     }
 
     /// <summary>
--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using UnityEngine.UI;
 public class Game : MonoBehaviour {
     public static float leaning { get; private set; }
+
+    public static float leaningAggregate;
+    public Text leaningText;
 	// Use this for initialization
 	void Start () {
-	
+        leaning = 0.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        float incr = Mathf.Log10(Mathf.Abs(leaningAggregate) + 1) * Time.deltaTime * 0.1f;
+        if (leaningAggregate < 0.0f)
+            incr *= -1.0f;
+        leaning += incr;
+        Debug.Log(leaning);
+        if(Input.GetKeyDown(KeyCode.A)) {
+            leaning -= 0.1f;
+        } else if (Input.GetKeyDown(KeyCode.D))
+        {
+            leaning += 0.1f;
+        }
+        leaning = Mathf.Clamp(leaning, -1.0f, 1.0f);
+        leaningText.text = leaning.ToString();
+        leaningAggregate = 0.0f;
+    }
+
+    void LateUpdate()
+    {
+        
+    }
 }
